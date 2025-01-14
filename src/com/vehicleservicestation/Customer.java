782536e4da07e1215d2067e6488ca5a6018c1474
santosh-ajabe <120372941+santosh-ajabe@users.noreply.github.com>
2025-01-14 09:42:36 +0530

    package created

---
+++ src/com/vehicleservicestation/Customer.java
@@ -0,0 +1,68 @@
+package com.vehicleservicestation;
+
+
+public  class Customer implements Comparable<Customer> {
+
+	public int compareTo(String anotherString) {
+		return address.compareTo(anotherString);
+	}
+
+	private String cname;
+	private int mobno;
+	private String email;
+	private String address;
+	
+	public Customer(String cname, int mobno, String email, String address) {
+		super();
+		this.cname = cname;
+		this.mobno = mobno;
+		this.email = email;
+		this.address = address;
+	}
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+
+	public long getMobno() {
+		return mobno;
+	}
+
+	public void setMobno(int mobno) {
+		this.mobno = mobno;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [cname=" + cname + ", mobno=" + mobno + ", email=" + email + ", address=" + address + "]";
+	}
+
+	@Override
+	public int compareTo(Customer o) {
+		 return Integer.compare(this.mobno,o.mobno);
+	}
+
+
+	
+	
+}
---
+++ src/com/vehicleservicestation/Vehicle.java
@@ -0,0 +1,5 @@
+package com.vehicleservicestation;
+
+public class Vehicle {
+
+}
