1ac16adca05b24b876ac9ea1fb3cccdbcb78c7a2
Nitin Baswane <nitinbaswane22@gmail.com>
2025-01-15 10:07:56 +0530

    new edit

--- src/com/manageoperation/VehicleOp.java
+++ src/com/manageoperation/VehicleOp.java
@@ -1,5 +1,28 @@
 package com.manageoperation;
 
-public class VehicleOp {
+import com.vehicleservicestation.Vehicle;
+
+public class VehicleOp extends Vehicle{
+
+	public VehicleOp(String manufacturer, String model, String vehicleNumber) {
+		super(manufacturer, model, vehicleNumber);
+		// TODO Auto-generated constructor stub
+	}
+    public void addVehicle() {
+		
+	}
+    public void displayAllVehicle() {
+		
+	}
+    public void displayCustomerVehicle() {
+		
+	}
+	public void deleteVehicle() {
+		
+	}
+	public void editVehicle() {
+		
+	}
+	
 
 }
--- src/com/vehicleservicestation/Vehicle.java
+++ src/com/vehicleservicestation/Vehicle.java
@@ -1,5 +1,44 @@
 package com.vehicleservicestation;
+import java.util.Objects;
 
-public class Vehicle {
+public class Vehicle implements Comparable<Vehicle>{
+	 private String manufacturer;
+	    private String model;
+	    private String vehicleNumber;
+		public Vehicle(String manufacturer, String model, String vehicleNumber) {
+			super();
+			this.manufacturer = manufacturer;
+			this.model = model;
+			this.vehicleNumber = vehicleNumber;
+		}
+		public String getManufacturer() {
+			return manufacturer;
+		}
+		public void setManufacturer(String manufacturer) {
+			this.manufacturer = manufacturer;
+		}
+		public String getModel() {
+			return model;
+		}
+		public void setModel(String model) {
+			this.model = model;
+		}
+		public String getVehicleNumber() {
+			return vehicleNumber;
+		}
+		public void setVehicleNumber(String vehicleNumber) {
+			this.vehicleNumber = vehicleNumber;
+		}
+		@Override
+		public String toString() {
+			return "Vehicle [manufacturer=" + manufacturer + ", model=" + model + ", vehicleNumber=" + vehicleNumber
+					+ "]";
+		}
+		@Override
+		public int compareTo(Vehicle o) {
+		// TODO Auto-generated method stub
+			return this.getVehicleNumber().compareTo(o.vehicleNumber );
+		}
+	    
 
 }
