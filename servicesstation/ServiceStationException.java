c7dfd086dfe92be91aa89880da82954f76de26fa
Nitin Baswane <nitinbaswane22@gmail.com>
2025-01-23 10:08:03 +0530

    chaged file structure

---
+++ servicesstation/ServiceStationException.java
@@ -0,0 +1,7 @@
+package servicesstation;
+
+class ServiceStationException extends Exception {
+    public ServiceStationException(String message) {
+        super(message);
+    }
+}
--- src/com/vehicleservicestation/Bill.java
+++
@@ -1,5 +0,0 @@
-package src.com.vehicleservicestation;
-
-public class Bill {
-
-}
